d48a21a9ff2d659ef25cd3ee3e548ff0a00d16a7
Jamal1463 <mohd.jama412@gmail.com>
2024-11-21 15:54:23 +0530

    update init files

--- eks-module/modules/eks/main.tf
+++ eks-module/modules/eks/main.tf
@@ -10,12 +10,12 @@ module "eks" {
 
   cluster_addons = {
     aws-ebs-csi-driver = {
-      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+      service_account_role_arn = "../module.irsa-ebs-csi.iam_role_arn"
     }
   }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id     = "../vpc/module.vpc.vpc_id"
+  subnet_ids = "../vpc/module.vpc.private_subnets"
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
