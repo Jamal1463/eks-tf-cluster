ae47665235c501e0bc665072b6c6758436dbc2f3
Jamal1463 <mohd.jama412@gmail.com>
2024-11-21 15:22:05 +0530

    Update data-provider.tf

--- eks-module/modules/data-provider.tf
+++ eks-module/modules/data-provider.tf
@@ -17,7 +17,7 @@ module "irsa-ebs-csi" {
   version = "5.39.0"
 
   create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  role_name                     = "../eks/AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
